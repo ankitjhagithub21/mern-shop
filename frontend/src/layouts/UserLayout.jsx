33d1403ac925e1276125c0d6f43ca65faf70b957
ankit <ajha45075@gmail.com>
2025-10-19 23:43:28 +0530

    added footer component

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { useCartStore } from '../store/useCartStore';
 import { useAuth } from '../context/AuthContext';
+import Footer from '../components/Footer';
 
 const UserLayout = ({ children }) => {
   const fetchCart = useCartStore(state => state.fetchCart);
@@ -17,6 +18,7 @@ const UserLayout = ({ children }) => {
     <>
       <Navbar />
       {children}
+      <Footer/>
     </>
   );
 };
